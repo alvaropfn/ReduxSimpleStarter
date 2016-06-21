21df9dcf90d1ff304cd0bcc01ea9e32ebdc572df
alvaropfn <alvaropfn@gmail.com>
2016-06-21 17:58:21 -0300

    adição dos componentes e teste bem sucedido da injeção da primeira propriedade

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyAtvWs0uEMpwZ8XqyODJka88RW69Ba40cg';
 
 //create new component should produce HTML
-const App = () =>
-{
-	return (
-		<div>hello</div>
-	);
+class App extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'HotS'},
+		(videos) => {this.setState({videos});
+		});
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos}/>
+			</div>
+		);
+	}
 }
 
 //put html genereted into de DOM
